ab706357ac6dcda2b60ec0bdb864b49388151b9b
diazbetancur <49962109+diazbetancur@users.noreply.github.com>
2021-02-18 21:19:18 -0500

    Update App.js
    Delete button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, {useState}from 'react'
-import { isEmpty } from 'loadsh'
+import React, { useState } from 'react'
+import { isEmpty, size } from 'loadsh'
 import shortId from 'shortid'
 
 function App() {
-   const [task, setTask] = useState("")
-   const [tasks, setTasks] = useState([])
+  const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
 
-   const addTask =(e) => {
-     e.preventDefault()
-     if(isEmpty(task))
-     {
-        console.log("Task empty")
-        return
-     }
-     
-     const newTask =
-     {
-        id:shortId.generate(),
-        name: task 
-     }
+  const addTask = (e) => {
+    e.preventDefault()
+    if (isEmpty(task)) {
+      console.log("Task empty")
+      return
+    }
 
-     setTasks([...tasks, newTask])
-     setTask("")
-   }
+    const newTask =
+    {
+      id: shortId.generate(),
+      name: task
+    }
+
+    setTasks([...tasks, newTask])
+    setTask("")
+  }
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(x => x.id != id))
+  }
 
   return (
     <div className="container mt-5">
@@ -31,29 +34,41 @@ function App() {
       <div className="row">
         <div className="col-8">
           <h4 className="text-center">Task List</h4>
-          <ul className="list-group">
-{
-  tasks.map((task) => (
-            <li className="list-group-item" key={task.id}>
-              <spam className="lead">{task.name}</spam>
-              <button className="btn btn-danger btn-sm float-right mx-2">Delete</button>
-              <button className="btn btn-warning btn-sm float-right">Edit</button>
-            </li>))
-}
-          </ul>
+          {
+            size(tasks) === 0 ? (
+              <h5 className="text-center">there are no scheduled tasks yet</h5>
+            ) : (
+                <ul className="list-group">
+                  {
+                    tasks.map((task) => (
+                      <li className="list-group-item" key={task.id}>
+                        <spam className="lead">{task.name}</spam>
+                        <button
+                          className="btn btn-danger btn-sm float-right mx-2"
+                          onClick={() => deleteTask(task.id)}
+                        >Delete
+              </button>
+                        <button
+                          className="btn btn-warning btn-sm float-right"
+                        >Edit
+              </button>
+                      </li>))
+                  }
+                </ul>)
+          }
         </div>
         <div className="col-4">
           <h4 className="text-center">Form</h4>
           <form onSubmit={addTask}>
-            <input type="text" 
-            className="form-control mb-2" 
-            placeholder="Insert task..."
-            onChange={(text) => setTask(text.target.value)}  
-            value={task}
+            <input type="text"
+              className="form-control mb-2"
+              placeholder="Insert task..."
+              onChange={(text) => setTask(text.target.value)}
+              value={task}
             ></input>
-            <button 
-            className="btn btn-dark btn-block"
-            type="submit">
+            <button
+              className="btn btn-dark btn-block"
+              type="submit">
               Add
             </button>
           </form>
